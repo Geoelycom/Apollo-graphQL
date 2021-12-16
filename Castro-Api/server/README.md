1ab1df746ed89a12163b9723558789d975374924
Elyan <ekenimohelyanpheta@gmail.com>
2021-12-15 17:42:44 -0800

    add schema file

---
+++ Castro-Api/server/README.md
@@ -0,0 +1,2 @@
+# Catstronauts - server
+The starting point of the `server` code for Odyssey Lift-off I course.
---
+++ Castro-Api/server/src/schema.js
@@ -0,0 +1,28 @@
+//TODO
+const { gql } = require('apollo-server');
+const typeDefs = gql `
+  #schema definations go here
+  type Query {
+    "Get tracks array for homepage grid"
+    tracksForHome: [Track!]!
+  }
+
+"A track is a group of Modules that teaches about a specific topic"
+  type Track {
+    id: ID!
+    title: String!
+    author: Author!
+    thumbnail: String
+    length: Int
+    modulesCount: Int
+  }
+
+  "Author of a complete Track"
+  type Author {
+    id: ID!
+    name: String!
+    photo: String
+  }
+`;
+
+module.exports = typeDefs;
