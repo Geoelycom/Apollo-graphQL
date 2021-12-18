5258caad4641ab6c9b5cefcc9c89ebe30ae9827a
Elyan <ekenimohelyanpheta@gmail.com>
2021-12-17 16:15:34 -0800

    Add pages

---
+++ Castro-Api/client/src/pages/tracks.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { Layout, QueryResult } from '../components';
+import { useQuery, gql } from '@apollo/client';
+import TrackCard from "../containers/track-card";
+
+/**
+ * Tracks Page is the Catstronauts home page.
+ * We display a grid of tracks fetched with useQuery with the TRACKS query
+ */
+
+
+export const TRACKS = gql`
+query ExampleQuery {
+  tracksForHome {
+    id
+    title
+    thumbnail
+    length
+    modulesCount
+    author {
+      id
+      name
+      photo
+    }
+  }
+}
+`;
+
+
+const Tracks = () => {
+  const { loading, error, data } = useQuery(TRACKS);
+
+  return ( 
+    <Layout grid>
+        <QueryResult error={error} loading={loading} data={data}>
+        {data?.tracksForHome?.map((track, index) => (
+          <TrackCard key={track.id} track={track} />
+        ))}
+        </QueryResult>
+    </Layout>
+  ) 
+   
+};
+
+export default Tracks;
